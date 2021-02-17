2b825e87d23a41a6a339617bc2383c3bff5378b0
Maxim Bogdanov <maxim.bogdanov@coins.ph>
2021-02-17 17:18:02 +0100

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::marker::PhantomData;
 
+mod cmp;
+
 #[derive(Default)]
 struct Context {
     row_id: usize,
@@ -186,23 +188,6 @@ impl Node for Then {
     }
 }
 
-trait Cmp<T> {
-    fn is_true(left: T, right: T) -> bool;
-    fn is_false(left: T, right: T) -> bool;
-}
-
-struct Equal;
-
-impl Cmp<u32> for Equal {
-    fn is_true(left: u32, right: u32) -> bool {
-        left == right
-    }
-
-    fn is_false(left: u32, right: u32) -> bool {
-        left != right
-    }
-}
-
 struct ScalarValue<T, C> {
     c: PhantomData<C>,
     state: Option<NodeState>,
@@ -211,7 +196,7 @@ struct ScalarValue<T, C> {
     right: T,
 }
 
-impl<T, C> Node for ScalarValue<T, C> where T: Copy, C: Cmp<T> {
+impl<T, C> Node for ScalarValue<T, C> where T: Copy, C: cmp::Cmp<T> {
     fn evaluate(&mut self, _: &Context) -> EvalResult {
         // check if node already has state
         if let Some(state) = &self.state {
@@ -247,7 +232,7 @@ struct VectorValue<T, C> {
     right: T,
 }
 
-impl<T, C> Node for VectorValue<T, C> where T: Copy, C: Cmp<T> {
+impl<T, C> Node for VectorValue<T, C> where T: Copy, C: cmp::Cmp<T> {
     fn evaluate(&mut self, ctx: &Context) -> EvalResult {
         // check if node already has state
         if let Some(state) = &self.state {
@@ -281,7 +266,7 @@ mod tests {
 
     #[test]
     fn scalar_value_equal_fails() {
-        let v: ScalarValue<u32, Equal> = ScalarValue {
+        let v: ScalarValue<u32, cmp::Equal> = ScalarValue {
             c: PhantomData,
             state: None,
             is_partition: false,
@@ -295,14 +280,84 @@ mod tests {
 
         assert_eq!(n.evaluate(&ctx), EvalResult::False(false))
     }
-    /*    #[test]
-        fn a_and_b() {
-            let mut q: Box<dyn Node> = Box::new(And {
-                state: None,
-                children: None,
-                limits: None
-            }),
-        }*/
+
+    #[test]
+    fn scalar_value_equal() {
+        let v: ScalarValue<u32, cmp::Equal> = ScalarValue {
+            c: PhantomData,
+            state: None,
+            is_partition: false,
+            left: 1,
+            right: 1,
+        };
+
+        let mut n: Box<dyn Node> = Box::new(v);
+
+        let ctx = Context::default();
+
+        assert_eq!(n.evaluate(&ctx), EvalResult::True(false))
+    }
+
+    #[test]
+    fn vector_value_equal_fails() {
+        let v: VectorValue<u32, cmp::Equal> = VectorValue {
+            c: PhantomData,
+            state: None,
+            is_partition: false,
+            left: vec![0, 1],
+            right: 1,
+        };
+
+        let mut n: Box<dyn Node> = Box::new(v);
+
+        let ctx = Context { row_id: 0 };
+
+        assert_eq!(n.evaluate(&ctx), EvalResult::False(false))
+    }
+
+    #[test]
+    fn vector_value_equal() {
+        let v: VectorValue<u32, cmp::Equal> = VectorValue {
+            c: PhantomData,
+            state: None,
+            is_partition: false,
+            left: vec![0, 1],
+            right: 1,
+        };
+
+        let mut n: Box<dyn Node> = Box::new(v);
+
+        let ctx = Context { row_id: 1 };
+
+        assert_eq!(n.evaluate(&ctx), EvalResult::True(false))
+    }
+
+    #[test]
+    fn a_and_b() {
+        let mut q: Box<dyn Node> = Box::new(And {
+            state: None,
+            children: Some(vec![
+                Box::new(ScalarValue::<u32, cmp::Equal> {
+                    c: PhantomData,
+                    state: None,
+                    is_partition: false,
+                    left: 1,
+                    right: 1,
+                }),
+                Box::new(ScalarValue::<u32, cmp::Equal> {
+                    c: PhantomData,
+                    state: None,
+                    is_partition: false,
+                    left: 2,
+                    right: 2,
+                })
+            ]),
+            limits: None,
+        });
+
+        let ctx = Context::default();
+        assert_eq!(q.evaluate(&ctx), EvalResult::True(false))
+    }
 }
 
 fn main() {
