120dbc1a4f9f785be58c25d54b957f997ee00578
IncSW <dev@incsw.in>
2021-08-25 20:49:24 +0300

    add hello world

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer};
+use std::env::var;
+
+#[get("/")]
+async fn index() -> &'static str {
+    "Hello, World!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // let user_service = web::Data::new(services::user::Service::new(pool.clone()).await.unwrap());
+    // let auth_service = web::Data::new(services::auth::Service::new(
+    //     user_service.clone().into_inner(),
+    // ));
+
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            // .app_data(user_service.clone())
+            // .configure(services::user::endpoints)
+            // .app_data(auth_service.clone())
+            // .configure(services::auth::endpoints)
+    })
+    .bind(var("ET_BIND_ADDRESS").unwrap())?
+    .run()
+    .await
 }
