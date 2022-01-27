1af7bc79d2251b01855955aa9d9396c65fb99e1b
IncSW <dev@incsw.in>
2022-01-28 01:18:23 +0300

    rwlock with boxed pointer

--- metadata/src/accounts/provider.rs
+++ metadata/src/accounts/provider.rs
@@ -4,8 +4,8 @@ use crate::store::store::Store;
 use crate::{Error, Result};
 use bincode::{deserialize, serialize};
 use chrono::Utc;
-use futures::lock::Mutex;
 use std::sync::Arc;
+use tokio::sync::RwLock;
 
 const NAMESPACE: &str = "accounts";
 const IDX_EMAIL: &str = "email";
@@ -48,32 +48,34 @@ impl<'a> Key<'a> {
 
 pub struct Provider {
     store: Arc<Store>,
-    idx: Mutex<HashMap>,
+    idx: *mut HashMap,
+    guard: RwLock<()>,
 }
+
+unsafe impl Send for Provider {}
+unsafe impl Sync for Provider {}
 
 impl Provider {
     pub fn new(kv: Arc<Store>) -> Self {
         Provider {
             store: kv.clone(),
-            idx: Mutex::new(HashMap::new(kv)),
+            idx: Box::leak(Box::new(HashMap::new(kv))),
+            guard: RwLock::new(()),
         }
     }
 
     pub async fn create(&self, req: CreateRequest) -> Result<Account> {
+        let _guard = self.guard.write().await;
+        let idx = unsafe { self.idx.as_mut().unwrap() };
         let idx_keys = index_keys(req.email.as_str());
-        let mut idx = self.idx.lock().await;
-
         idx.check_insert_constraints(idx_keys.as_ref()).await?;
 
         let created_at = Utc::now();
         let id = self.store.next_seq(Key::IdSequence.as_bytes()).await?;
-
         let account = req.into_account(id, created_at);
-
         self.store
             .put(Key::Data(account.id).as_bytes(), serialize(&account)?)
             .await?;
-
         idx.insert(idx_keys.as_ref(), account.id.to_le_bytes())
             .await?;
         Ok(account)
@@ -87,8 +89,8 @@ impl Provider {
     }
 
     pub async fn get_by_email(&self, email: &str) -> Result<Account> {
-        let idx = self.idx.lock().await;
-
+        let _guard = self.guard.read().await;
+        let idx = unsafe { self.idx.as_ref().unwrap() };
         let id = idx.get(Key::Index(IDX_EMAIL, email).as_bytes()).await?;
         self.get_by_id(u64::from_le_bytes(id.try_into()?)).await
     }
@@ -107,40 +109,33 @@ impl Provider {
     }
 
     pub async fn update(&self, req: UpdateRequest) -> Result<Account> {
+        let _guard = self.guard.write().await;
+        let idx = unsafe { self.idx.as_mut().unwrap() };
         let prev_account = self.get_by_id(req.id).await?;
         let idx_keys = index_keys(req.email.as_str());
         let idx_prev_keys = index_keys(prev_account.email.as_str());
-
-        let mut idx = self.idx.lock().await;
-
         idx.check_update_constraints(idx_keys.as_ref(), idx_prev_keys.as_ref())
             .await?;
-
         let updated_at = Utc::now(); // TODO add updated_by
         let account = req.into_account(prev_account, updated_at, None);
-
         self.store
             .put(Key::Data(account.id).as_bytes(), serialize(&account)?)
             .await?;
-
         idx.update(
             idx_keys.as_ref(),
             idx_prev_keys.as_ref(),
             account.id.to_le_bytes(),
         )
         .await?;
-
         Ok(account)
     }
 
     pub async fn delete(&self, id: u64) -> Result<Account> {
+        let _guard = self.guard.write().await;
+        let idx = unsafe { self.idx.as_mut().unwrap() };
         let account = self.get_by_id(id).await?;
         self.store.delete(Key::Data(id).as_bytes()).await?;
-
-        self.idx
-            .lock()
-            .await
-            .delete(index_keys(account.email.as_str()).as_ref())
+        idx.delete(index_keys(account.email.as_str()).as_ref())
             .await?;
         Ok(account)
     }
