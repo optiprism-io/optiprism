b30e20f93666d99a50e0ae1aca81e67ca9827a7f
Sergey Melnychuk <serg.meln@gmail.com>
2022-02-12 23:29:58 +0100

    Build arrays for benchmark in advance

--- query/benches/sorted_distinct_count.rs
+++ query/benches/sorted_distinct_count.rs
@@ -22,10 +22,14 @@ fn new_random_array(size: usize, limit: usize) -> ArrayRef {
 pub fn sorted_distinct_count_bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("sorted_distinct_count");
 
-    for size in [10, 20, 40, 100, 200, 400, 1000] {
+    let arrays = [10, 20, 40, 100, 200, 400, 1000].into_iter()
+        .map(|size| new_random_array(size, size / 2))
+        .collect::<Vec<_>>();
+
+    for array in arrays {
+        let size = array.len() as u64;
+        group.throughput(Throughput::Elements(size));
         let id = BenchmarkId::from_parameter(size);
-        let array = new_random_array(size, size / 2);
-        group.throughput(Throughput::Elements(size as u64));
         group.bench_with_input(id, &array, |b, array| {
             let mut acc = SortedDistinctCountAccumulator::try_new(&DataType::Int64).unwrap();
             b.iter(move || {
