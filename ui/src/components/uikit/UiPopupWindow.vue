21bd70056ae869d7fe0a2e1dd4a2317e6aa12578
Nikolay Kushner <nickolaikushner@gmail.com>
2022-03-27 20:15:03 +0300

    create UiPopupWindow component

---
+++ ui/src/components/uikit/UiPopupWindow.vue
@@ -0,0 +1,198 @@
+<template>
+    <div class="ui-popup-window">
+        <div
+            class="ui-popup-window__wrapper"
+            ref="popupWrapper"
+        >
+            <div
+                class="pf-c-modal-box"
+                :class="props.size"
+                aria-modal="true"
+                aria-labelledby="modal-title"
+                aria-describedby="modal-description"
+                v-click-outside="onClickOutside"
+            >
+                <button
+                    v-if="closable"
+                    class="pf-c-button pf-m-plain"
+                    type="button"
+                    aria-label="Close"
+                    @click="cancel('close-btn')"
+                >
+                    <i class="fas fa-times" aria-hidden="true"></i>
+                </button>
+                <header
+                    v-if="props.title"
+                    class="pf-c-modal-box__header"
+                >
+                    <h1
+                        class="pf-c-modal-box__title"
+                        id="modal-title"
+                    >
+                        {{ props.title }}
+                    </h1>
+                    <div
+                        v-id="props.description"
+                        class="pf-c-modal-box__description"
+                    >
+                        {{ props.description }}
+                    </div>
+                </header>
+                <div class="pf-c-modal-box__body">
+                    <slot></slot>
+                </div>
+                <footer
+                    v-if="props.applyButton || props.cancelButton"
+                    class="pf-c-modal-box__footer"
+                >
+                    <div class="pf-c-action-list">
+                        <div
+                            v-if="props.applyButton"
+                            class="pf-c-action-list__item"
+                        >
+                            <UiButton
+                                class="pf-m-main pf-m-primary"
+                                type="button"
+                                :progress="props.applyLoading"
+                                @click="cancel('cancel-button')"
+                            >
+                                {{ props.applyButton }}
+                            </UiButton>
+                        </div>
+                        <div
+                            v-if="props.cancelButton"
+                            class="pf-c-action-list__item"
+                        >
+                            <UiButton
+                                class=""
+                                type="button"
+                                @click="apply"
+                            >
+                                {{ props.cancelButton }}
+                            </UiButton>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, inject, onMounted, onUnmounted } from 'vue'
+
+import getScrollbarWidth from '@/helpers/getScrollbarWidth'
+
+import UiButton from '@/components/uikit/UiButton.vue'
+
+interface Props {
+    title?: string
+    description?: string
+    closable?: boolean
+
+    applyButton?: string
+    cancelButton?: string
+
+    applyLoading?: boolean
+
+    saveBodyOverflow?: boolean
+    noRemoveBodyOverflow?: boolean
+
+    size?: 'pf-m-md' | 'pf-m-sm' | 'pf-m-lg' | null
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    size: 'pf-m-sm',
+    closable: true,
+})
+
+const emit = defineEmits<{
+    (e: 'cancel', turget: string): void
+    (e: 'apply'): void
+    (e: 'open'): void
+}>()
+
+
+const popupWrapper = ref<HTMLDivElement>()
+const initialBodyOverflow = ref('')
+
+const onClickOutside = (e: MouseEvent) => {
+    const popupWrapperElement = popupWrapper.value
+    const clickScroll = popupWrapperElement && popupWrapperElement.clientWidth < e.x
+
+    if (!clickScroll) {
+        emit('cancel', 'outside')
+    }
+}
+
+const cancel = (target: string) => {
+    emit('cancel', target)
+}
+
+const apply = () => {
+    emit('apply')
+}
+
+const escHandler = (e: KeyboardEvent) => {
+    const ESC = 27;
+
+    if(e.which === ESC) {
+        e.stopImmediatePropagation();
+        cancel('esc');
+    }
+}
+
+
+onMounted(() => {
+    if (props.saveBodyOverflow) {
+        initialBodyOverflow.value = document.body.style.overflow
+    }
+
+    document.body.style.overflow = 'hidden'
+
+    if (document.body.scrollHeight > window.innerHeight) {
+        document.body.style.paddingRight = `${getScrollbarWidth()}px`
+    }
+    emit('open')
+
+    document.documentElement.addEventListener('keydown', escHandler, true)
+})
+
+onUnmounted(() => {
+    if (document.body.scrollHeight > window.innerHeight) {
+        document.body.style.removeProperty('padding')
+    }
+
+    if (!props.noRemoveBodyOverflow) {
+        document.body.style.removeProperty('overflow')
+    }
+
+    document.documentElement.removeEventListener('keydown', escHandler, true)
+
+    if (props.saveBodyOverflow) {
+        document.body.style.overflow = initialBodyOverflow.value
+    }
+})
+
+</script>
+<style lang="scss">
+$text-color: #171B24;
+$text-color-title: #171717;
+
+.ui-popup-window {
+    &__wrapper {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        display: flex;
+        justify-content: center;
+        top: 0;
+        left: 0;
+        background-color: rgba(23, 23, 23, 0.7);
+        overflow: auto;
+        padding-top: 2rem;
+        padding-bottom: 2rem;
+        z-index: 1800;
+    }
+}
+</style>
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -25,6 +25,21 @@ app.use(uikitPlugin);
 app.use(i18nPlugin, lang.en);
 app.use(router);
 
+
+app.directive('click-outside', {
+    mounted(el, binding, vnode) {
+      el.clickOutsideEvent = function(event: Event) {
+        if (!(el === event.target || el.contains(event.target))) {
+          binding.value(event, el)
+        }
+      }
+      document.body.addEventListener('click', el.clickOutsideEvent)
+    },
+    unmounted(el) {
+      document.body.removeEventListener('click', el.clickOutsideEvent)
+    }
+})
+
 app.mount("#app");
 
 app.config.errorHandler = (err, vm, info) => {
