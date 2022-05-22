feace25e78fa0ff5580ae7cfdd1ab5bfc19c8ee5
Maxim Bogdanov <maxim.bogdanov@coins.ph>
2022-05-22 22:28:04 +0200

    wip

--- events-gen/src/bin/store/main.rs
+++ events-gen/src/bin/store/main.rs
@@ -3,13 +3,13 @@ use chrono::{DateTime, Utc};
 use rand::thread_rng;
 use events_gen::generator::Generator;
 use events_gen::store::products::{Preferences, Products};
-use events_gen::store::scenario::{run};
+use events_gen::store::scenario::{RecordBatchBuilder, run};
 
 fn main() {
     let mut rng = thread_rng();
     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     path.push("src/store/data/products.csv");
-    let mut products = Products::try_new_from_csv(path, rng.clone()).unwrap();
+    let mut products = Products::try_new_from_csv(path, &mut rng).unwrap();
     let preferences = Preferences {
         categories: None,
         subcategories: None,
@@ -23,12 +23,14 @@ fn main() {
         has_coupon: true,
     };
 
-    let to = DateTime::parse_from_rfc3339("2021-09-08T14:42:00.000000+00:00").unwrap().with_timezone(&Utc);
+    let from = DateTime::parse_from_rfc3339("2021-09-08T13:42:00.000000+00:00").unwrap().with_timezone(&Utc);
+    let to = DateTime::parse_from_rfc3339("2022-09-08T14:42:00.000000+00:00").unwrap().with_timezone(&Utc);
     let mut gen = Generator::new(
         rng.clone(),
-        DateTime::parse_from_rfc3339("2021-09-08T13:42:00.000000+00:00").unwrap().with_timezone(&Utc),
+        from,
         to.clone(),
         5,
     );
-    run(&mut gen, &mut products, to, rng.clone()).unwrap();
+    let mut builder = RecordBatchBuilder::new();
+    run(&mut gen, &mut products, to, &mut builder, rng.clone()).unwrap();
 }
