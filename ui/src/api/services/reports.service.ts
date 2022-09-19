0ff07063077b5ee7b94c3edb22d09cf40b0323bb
Nikolay Kushner <nickolaikushner@gmail.com>
2022-09-19 21:08:31 +0300

    update service api

--- ui/src/api/services/reports.service.ts
+++ ui/src/api/services/reports.service.ts
@@ -1,9 +1,20 @@
-import {EventSegmentation, ReportsApi} from '@/api'
+import {
+    EventSegmentation,
+    ReportsApi,
+    CreateReportRequest,
+    UpdateReportRequest,
+} from '@/api'
 
 const api = new ReportsApi()
 
 const schemaReports = {
     eventSegmentation:  async(organizationId: number, projectId: number, eventSegmentation: EventSegmentation) => await api.eventSegmentationQuery(organizationId, projectId, eventSegmentation),
+
+    reportsList: async(organizationId: number, projectId: number) => await api.reportsList(organizationId, projectId),
+    getReport: async(organizationId: number, projectId: number, reportId: number) => await api.getReport(organizationId, projectId, reportId),
+    createReport: async(organizationId: number, projectId: number, createReportRequest: CreateReportRequest) => await api.createReport(organizationId, projectId, createReportRequest),
+    deleteReport: async(organizationId: number, projectId: number, reportId: number) => await api.deleteReport(organizationId, projectId, reportId),
+    updateReport: async(organizationId: number, projectId: number, updateReportRequest: UpdateReportRequest) => await api.updateReport(organizationId, projectId, updateReportRequest),
 }
 
 export default schemaReports
