183602ddd6cd56f9a5a5d5d2083d84f1bf695c63
Maxim Bogdanov <muravlion@gmail.com>
2022-10-22 20:16:15 +0200

    auto commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
         ecmaVersion: 2021
     },
     rules: {
-        quotes: ['warn', 'single'],
+        quotes: ['warn', 'single','f'],
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-duplicate-case': 'off',
