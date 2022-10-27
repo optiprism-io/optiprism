6aac2ff1cc318306926a60858a85fee5d6503ee9
Maxim Bogdanov <muravlion@gmail.com>
2022-10-27 14:06:23 +0200

    fixed tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
-import {defineConfig} from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
 
 export default defineConfig({
-    root: 'frontend',
-    plugins: [
-        vue(),
-        vueJsx(),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'frontend', 'src')
-        }
-    },
-    test: {
-        root: 'frontend'
-    }
+	plugins: [
+		vue(),
+		vueJsx(),
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'frontend', 'src')
+		}
+	},
+	test: {
+		include: ['frontend/test/unit/**/*.{test,spec}.ts'],
+	},
 })
