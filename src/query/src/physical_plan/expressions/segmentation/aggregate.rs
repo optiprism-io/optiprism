865cc1118f11b9fe7a38591f9037540c2b6f618b
Maxim Bogdanov <muravlion@gmail.com>
2023-06-28 18:51:54 +0200

    decimal for aggregate

--- src/query/src/physical_plan/expressions/segmentation/aggregate.rs
+++ src/query/src/physical_plan/expressions/segmentation/aggregate.rs
@@ -25,6 +25,8 @@ use arrow::array::UInt64Array;
 use arrow::array::UInt8Array;
 use arrow::record_batch::RecordBatch;
 use arrow2::types::f16;
+use common::DECIMAL_PRECISION;
+use common::DECIMAL_SCALE;
 use datafusion::physical_expr::expressions::Column;
 use datafusion::physical_expr::PhysicalExpr;
 use num_traits::Bounded;
@@ -57,9 +59,7 @@ where OT: Copy + Num + Bounded + NumCast + PartialOrd + Clone
 }
 
 impl<T, OT, OB> Aggregate<T, OT, OB>
-where
-    OT: Copy + Num + Bounded + NumCast + PartialOrd + Clone,
-    T: Copy,
+where OT: Copy + Num + Bounded + NumCast + PartialOrd + Clone
 {
     pub fn try_new(
         predicate: Column,
@@ -145,6 +145,79 @@ macro_rules! gen_agg_int {
     };
 }
 
+macro_rules! gen_agg_decimal {
+    ($ty:ident,$array_ty:ident,$acc_ty:ty,$acc_builder:ty) => {
+        impl SegmentationExpr for Aggregate<$ty, $acc_ty, $acc_builder> {
+            fn evaluate(
+                &self,
+                record_batch: &RecordBatch,
+                hashes: &[u64],
+            ) -> Result<Option<ArrayRef>> {
+                let ts = self
+                    .ts_col
+                    .evaluate(record_batch)?
+                    .into_array(record_batch.num_rows())
+                    .as_any()
+                    .downcast_ref::<TimestampMillisecondArray>()
+                    .unwrap()
+                    .clone();
+
+                let mut inner = self.inner.lock().unwrap();
+                let arr = self
+                    .predicate
+                    .evaluate(record_batch)?
+                    .into_array(record_batch.num_rows())
+                    .as_any()
+                    .downcast_ref::<$array_ty>()
+                    .unwrap()
+                    .clone()
+                    .with_precision_and_scale(DECIMAL_PRECISION, DECIMAL_SCALE)?;
+                println!("{:?}", arr);
+                for (idx, hash) in hashes.iter().enumerate() {
+                    if inner.last_hash == 0 {
+                        inner.last_hash = *hash;
+                    }
+                    if *hash != inner.last_hash {
+                        inner.last_hash = *hash;
+                        let res = inner.agg.result();
+                        inner.out.append_value(res);
+                        inner.agg.reset();
+                    }
+
+                    if !self.time_range.check_bounds(ts.value(idx)) {
+                        continue;
+                    }
+                    println!("{:?}", arr.value(idx));
+                    inner.agg.accumulate(arr.value(idx).into());
+                }
+
+                if inner.out.len() > 0 {
+                    Ok(Some(Arc::new(
+                        inner
+                            .out
+                            .finish()
+                            .with_precision_and_scale(DECIMAL_PRECISION, DECIMAL_SCALE)?,
+                    ) as ArrayRef))
+                } else {
+                    Ok(None)
+                }
+            }
+
+            fn finalize(&self) -> Result<ArrayRef> {
+                let mut inner = self.inner.lock().unwrap();
+                let res = inner.agg.result() as $acc_ty;
+                inner.out.append_value(res);
+                Ok(Arc::new(
+                    inner
+                        .out
+                        .finish()
+                        .with_precision_and_scale(DECIMAL_PRECISION, DECIMAL_SCALE)?,
+                ) as ArrayRef)
+            }
+        }
+    };
+}
+
 gen_agg_int!(i8, Int8Array, i64, Int64Builder);
 gen_agg_int!(i16, Int16Array, i64, Int64Builder);
 gen_agg_int!(i32, Int32Array, i64, Int64Builder);
@@ -157,6 +230,7 @@ gen_agg_int!(u64, Int64Array, i128, Decimal128Builder);
 gen_agg_int!(u128, Decimal128Array, i128, Decimal128Builder);
 gen_agg_int!(f32, Float32Array, f64, Float64Builder);
 gen_agg_int!(f64, Float64Array, f64, Float64Builder);
+gen_agg_decimal!(Decimal128Array, Decimal128Array, i128, Decimal128Builder);
 // todo add decimal 256
 // gen_agg_int!(i128, Decimal128Array);
 // gen_agg_int!(f64, Float32Array);
@@ -168,6 +242,8 @@ mod tests {
 
     use arrow::array::Array;
     use arrow::array::ArrayRef;
+    use arrow::array::Decimal128Array;
+    use arrow::array::Decimal128Builder;
     use arrow::array::Float16Array;
     use arrow::array::Float32Array;
     use arrow::array::Float32Builder;
@@ -183,6 +259,8 @@ mod tests {
     use arrow::datatypes::SchemaRef;
     use arrow::datatypes::TimeUnit;
     use arrow::record_batch::RecordBatch;
+    use common::DECIMAL_PRECISION;
+    use common::DECIMAL_SCALE;
     use datafusion::physical_expr::expressions::Column;
     use datafusion::physical_expr::hash_utils::create_hashes;
 
@@ -307,5 +385,77 @@ mod tests {
         let res = agg.finalize().unwrap();
         let right = Arc::new(Float64Array::from(vec![1.])) as ArrayRef;
         assert_eq!(&*res, &*right);
+    }
+
+    #[test]
+    fn test_sum_decimal() {
+        let schema = Schema::new(vec![
+            Field::new("col1", DataType::Int64, false),
+            Field::new(
+                "col2",
+                DataType::Decimal128(DECIMAL_PRECISION, DECIMAL_SCALE),
+                false,
+            ),
+            Field::new(
+                "ts",
+                DataType::Timestamp(TimeUnit::Millisecond, None),
+                false,
+            ),
+        ]);
+
+        let col1: ArrayRef = Arc::new(Int64Array::from(vec![1, 1, 1, 2, 2, 2, 3, 3, 3]));
+        let col2 = {
+            let vals: Vec<i128> = vec![123, 231, 314, 411, 523, 623, 713, 843, 91];
+            let mut builder = Decimal128Builder::with_capacity(9);
+            for val in vals.iter() {
+                builder.append_value(*val);
+            }
+
+            Arc::new(
+                builder
+                    .finish()
+                    .with_precision_and_scale(DECIMAL_PRECISION, DECIMAL_SCALE)
+                    .unwrap(),
+            )
+        };
+        let ts: ArrayRef = Arc::new(TimestampMillisecondArray::from(vec![
+            1, 2, 3, 1, 2, 3, 1, 2, 3,
+        ]));
+        let batch = RecordBatch::try_new(Arc::new(schema.clone()), vec![
+            col1.clone(),
+            col2.clone(),
+            ts.clone(),
+        ])
+        .unwrap();
+
+        let mut random_state = ahash::RandomState::with_seeds(0, 0, 0, 0);
+        let mut hash_buf = vec![];
+        hash_buf.resize(col1.len(), 0);
+        create_hashes(&vec![col1], &mut random_state, &mut hash_buf).unwrap();
+        let mut agg = Aggregate::<Decimal128Array, i128, _>::try_new(
+            Column::new_with_schema("col2", &schema).unwrap(),
+            AggregateFunction::new_sum(),
+            Decimal128Builder::with_capacity(10_000),
+            Column::new_with_schema("ts", &schema).unwrap(),
+            TimeRange::None,
+        )
+        .unwrap();
+
+        let res = agg.evaluate(&batch, &hash_buf).unwrap();
+        let exp = {
+            let vals: Vec<i128> = vec![668, 1557];
+            let mut builder = Decimal128Builder::with_capacity(9);
+            for val in vals.iter() {
+                builder.append_value(*val);
+            }
+
+            Arc::new(
+                builder
+                    .finish()
+                    .with_precision_and_scale(DECIMAL_PRECISION, DECIMAL_SCALE)
+                    .unwrap(),
+            ) as ArrayRef
+        };
+        assert_eq!(res, Some(exp));
     }
 }
--- src/query/src/physical_plan/planner.rs
+++ src/query/src/physical_plan/planner.rs
@@ -1,15 +1,18 @@
 use std::sync::Arc;
 
 use arrow::array::Float64Builder;
+use arrow::datatypes::DataType;
 use axum::async_trait;
 use datafusion::execution::context::QueryPlanner as DFQueryPlanner;
 use datafusion::execution::context::SessionState;
+use datafusion::physical_expr::PhysicalExpr;
 use datafusion::physical_plan::expressions;
 use datafusion::physical_plan::planner::DefaultPhysicalPlanner;
 use datafusion::physical_plan::planner::ExtensionPlanner as DFExtensionPlanner;
 use datafusion::physical_plan::ExecutionPlan;
 use datafusion::physical_plan::PhysicalPlanner;
 use datafusion_common::DataFusionError;
+use datafusion_common::ExprSchema;
 use datafusion_common::Result;
 use datafusion_common::Result as DFResult;
 use datafusion_expr::LogicalPlan;
@@ -24,6 +27,7 @@ use crate::logical_plan::segmentation::TimeRange;
 use crate::logical_plan::unpivot::UnpivotNode;
 use crate::physical_plan::dictionary_decode::DictionaryDecodeExec;
 use crate::physical_plan::expressions::segmentation::aggregate::Aggregate;
+use crate::physical_plan::expressions::segmentation::count::Count;
 use crate::physical_plan::expressions::segmentation::time_range::TimeRange as SegmentTimeRange;
 use crate::physical_plan::expressions::segmentation::AggregateFunction as SegmentAggregateFunction;
 use crate::physical_plan::expressions::segmentation::SegmentationExpr;
@@ -108,7 +112,7 @@ impl DFExtensionPlanner for ExtensionPlanner {
                 .collect();
             let exec = DictionaryDecodeExec::new(physical_inputs[0].clone(), decode_cols);
             Some(Arc::new(exec) as Arc<dyn ExecutionPlan>)
-        } else if let Some(node) = any.downcast_ref::<SegmentationNode>() {
+        /*} else if let Some(node) = any.downcast_ref::<SegmentationNode>() {
             let mut exprs: Vec<Arc<dyn SegmentationExpr>> = Vec::with_capacity(node.exprs.len());
             for expr in node.exprs.iter() {
                 let time_range = match &expr.time_range {
@@ -121,19 +125,22 @@ impl DFExtensionPlanner for ExtensionPlanner {
                         }
                     },
                 };
+                let ts_col =
+                    expressions::Column::new("ts", node.schema.index_of_column(&node.ts_col)?);
                 let res = match &expr.agg_fn {
-                    AggregateFunction::Sum(predicate) => Aggregate::<f32, f64, _>::try_new(
-                        expressions::Column::new(
-                            predicate.name.as_str(),
-                            node.input.schema().index_of_column(&predicate)?,
-                        ),
-                        SegmentAggregateFunction::new_sum(),
-                        Float64Builder::with_capacity(10_000),
-                        expressions::Column::new("ts", node.schema.index_of_column(&node.ts_col)?),
-                        time_range,
-                    )
-                    .unwrap(),
-                    _ => unimplemented!(),
+                    AggregateFunction::Sum(predicate) => {
+                        agg_segment_expr!(node.input.schema(), predicate, new_sum)
+                    }
+                    AggregateFunction::Min(_) => {
+                        agg_segment_expr!(node.input.schema(), predicate, new_min)
+                    }
+                    AggregateFunction::Max(_) => {
+                        agg_segment_expr!(node.input.schema(), predicate, new_max)
+                    }
+                    AggregateFunction::Avg(_) => {
+                        agg_segment_expr!(node.input.schema(), predicate, new_avg)
+                    }
+                    AggregateFunction::Count => Count::new(ts_col, time_range),
                 };
                 exprs.push(Arc::new(res))
             }
@@ -151,10 +158,46 @@ impl DFExtensionPlanner for ExtensionPlanner {
             let exec = SegmentationExec::try_new(exprs, cols, physical_inputs[0].clone(), 100)
                 .map_err(|err| DataFusionError::Plan(err.to_string()))?;
 
-            Some(Arc::new(exec) as Arc<dyn ExecutionPlan>)
+            Some(Arc::new(exec) as Arc<dyn ExecutionPlan>)*/
         } else {
             None
         };
         Ok(plan)
     }
 }
+// macro_rules! agg_expr {
+// ($t:typ,$acc:typ,$agg_fn:ident,$builder:ident) => {
+// Aggregate::<$t, $acc, _>::try_new(
+// predicate_expr,
+// SegmentAggregateFunction::$agg_fn(),
+// $builder::with_capacity(10_000),
+// ts_col,
+// time_range,
+// )?
+// };
+// }
+// macro_rules! agg_segment_expr {
+// ($schema:expr,$predicate:expr,$agg_fn:ident) => {
+// let predicate_expr = expressions::Column::new(
+// $predicate.name.as_str(),
+// $schema.index_of_column(&predicate)?,
+// );
+// match $schema.data_type(predicate)? {
+// DataType::Int8 => agg_expr!(i8, i64, $agg_fn, Int64Builder),
+// DataType::Int16 => agg_expr!(i16, i64, $agg_fn, Int64Builder),
+// DataType::Int32 => agg_expr!(i32, i64, $agg_fn, Int64Builder),
+// DataType::Int64 => agg_expr!(i32, i128, $agg_fn, Decimal128Builder),
+// DataType::UInt8 => agg_expr!(u8, i64, $agg_fn, Int64Builder),
+// DataType::UInt16 => agg_expr!(u16, i64, $agg_fn, Int64Builder),
+// DataType::UInt32 => agg_expr!(u32, i64, $agg_fn, Int64Builder),
+// DataType::UInt64 => agg_expr!(u64, i64, $agg_fn, Int64Builder),
+// DataType::Float32 => agg_expr!(f32, f64, $agg_fn, Float64Builder),
+// DataType::Float64 => agg_expr!(f64, f64, $agg_fn, Float64Builder),
+// DataType::Dictionary(p, s) => {}
+// DataType::Decimal128(_, _) => {}
+// DataType::Decimal256(_, _) => {}
+// DataType::Map(_, _) => {}
+// DataType::RunEndEncoded(_, _) => {}
+// }
+// };
+// }
