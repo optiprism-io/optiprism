0b5878ab325303051943489f68067f6738e7d6ed
Maxim Bogdanov <muravlion@gmail.com>
2023-07-14 12:25:35 +0200

    store tests

--- src/store/src/lib.rs
+++ src/store/src/lib.rs
@@ -747,45 +747,45 @@ pub mod test_util {
         array.into()
     }
 
-    /// Parses a markdown table into a vector of arrays:
-    ///  * Types supported: Int64, Int32, Float64, Boolean, Utf8, List
-    ///  * List types supported: Int64, Int32, Float64, Boolean, Utf8
-    ///  * List should be only one level deep
-    ///
-    /// # Arguments
-    /// * `data`   - The markdown table. The first row must be the header. You can use a list of values
-    ///              separated by commas to create a list array
-    /// * `fields` - The fields of the table
-    ///
-    /// # Example
-    ///     let data = r#"
-    /// ```markdown
-    /// | a | b     | c    | d     | e     |
-    /// |---|-------|------|-------|-------|
-    /// | 1 | true  | test | 1,2,3 | a,b,c |
-    /// | 2 |       |      | 1,2   | b     |
-    /// | 3 | false | lala |       |       |
-    /// ```
-    ///     "#;
-    ///     let fields = vec![
-    ///         Field::new("a", DataType::Int64, true),
-    ///         Field::new("b", DataType::Boolean, true),
-    ///         Field::new("c", DataType::Utf8, true),
-    ///         Field::new("d", DataType::List(Box::new(Field::new("1", DataType::Int32, true))), true),
-    ///         Field::new("e", DataType::List(Box::new(Field::new("1", DataType::Utf8, true))), true),
-    ///     ];
-    ///
-    ///     let parsed = parse_markdown_table(data.to_string(), &fields)?;
-    ///     println!("{:#?}", parsed);
-    ///
-    ///    // Output:
-    ///    // [
-    ///    //  Int64[1, 2, 3],
-    ///    //  BooleanArray[true, None, false],
-    ///    //  LargeUtf8Array[test, None, lala],
-    ///    //  ListArray[[1, 2, 3], [1, 2], None],
-    ///    //  ListArray[[a, b, c], [b], None],
-    ///    // ]
+    // Parses a markdown table into a vector of arrays:
+    //  * Types supported: Int64, Int32, Float64, Boolean, Utf8, List
+    //  * List types supported: Int64, Int32, Float64, Boolean, Utf8
+    //  * List should be only one level deep
+    //
+    // # Arguments
+    // * `data`   - The markdown table. The first row must be the header. You can use a list of values
+    //              separated by commas to create a list array
+    // * `fields` - The fields of the table
+    //
+    // # Example
+    //     let data = r#"
+    // ```markdown
+    // | a | b     | c    | d     | e     |
+    // |---|-------|------|-------|-------|
+    // | 1 | true  | test | 1,2,3 | a,b,c |
+    // | 2 |       |      | 1,2   | b     |
+    // | 3 | false | lala |       |       |
+    // ```
+    //     "#;
+    //     let fields = vec![
+    //         Field::new("a", DataType::Int64, true),
+    //         Field::new("b", DataType::Boolean, true),
+    //         Field::new("c", DataType::Utf8, true),
+    //         Field::new("d", DataType::List(Box::new(Field::new("1", DataType::Int32, true))), true),
+    //         Field::new("e", DataType::List(Box::new(Field::new("1", DataType::Utf8, true))), true),
+    //     ];
+    //
+    //     let parsed = parse_markdown_table(data.to_string(), &fields)?;
+    //     println!("{:#?}", parsed);
+    //
+    //    // Output:
+    //    // [
+    //    //  Int64[1, 2, 3],
+    //    //  BooleanArray[true, None, false],
+    //    //  LargeUtf8Array[test, None, lala],
+    //    //  ListArray[[1, 2, 3], [1, 2], None],
+    //    //  ListArray[[a, b, c], [b], None],
+    //    // ]
     pub fn parse_markdown_table(
         data: &str,
         fields: &[Field],
