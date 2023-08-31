52224a55a766ec3cd938f73bcf0d73159e4f7762
Maxim Bogdanov <muravlion@gmail.com>
2023-08-31 21:22:32 +0200

    .

--- src/events-gen/src/store/coefficients.rs
+++ src/events-gen/src/store/coefficients.rs
@@ -14,6 +14,7 @@ pub struct Coefficients {
     pub view_product_to_buy: f64,
     pub refund: f64,
     pub product_rating: f64,
+    f
 }
 
 pub fn make_coefficients(intention: &Intention) -> Coefficients {
