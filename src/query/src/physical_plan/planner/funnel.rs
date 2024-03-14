ecac11ae4104094175280669ec058dfd2bb4037b
Maxim Bogdanov <muravlion@gmail.com>
2024-03-14 09:37:49 +0100

    .

--- src/query/src/physical_plan/planner/funnel.rs
+++ src/query/src/physical_plan/planner/funnel.rs
@@ -1,8 +1,10 @@
 use std::sync::Arc;
-use crate::logical_plan::funnel::FunnelNode;
-use crate::physical_plan::funnel::{FunnelFinalExec, FunnelPartialExec};
+
 use crate::error::Result;
-pub(crate) fn funnel(node: &FunnelNode) ->Result<FunnelFinalExec> {
+use crate::logical_plan::funnel::FunnelNode;
+use crate::physical_plan::funnel::FunnelFinalExec;
+use crate::physical_plan::funnel::FunnelPartialExec;
+pub(crate) fn funnel(node: &FunnelNode) -> Result<FunnelFinalExec> {
     let partial = FunnelPartialExec::try_new()?;
 
     Ok(FunnelFinalExec::try_new(Arc::new(partial))?)
